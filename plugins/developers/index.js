9d7f96cf8d7e600d814b48b05af21cf957bb7dbd
surajdalvi1 <suraj.dalvi@raweng.com>
2017-09-13 17:17:23 +0530

    arvind god

--- plugins/developers/index.js
+++ plugins/developers/index.js
@@ -8,7 +8,8 @@
  * Module dependencies
  */
 var contentstack =  require('contentstack-express');
-
+var path =  require('path');
+console.log(contentstack.config.get('path').theme);
 module.exports = function Developers() {
     
 
@@ -18,13 +19,17 @@ module.exports = function Developers() {
     Developers.serverExtends = function(app) {
         app.get('/test',function(req, res, next){
             try{
-                res.set({
+                res.setHeader('content-disposition','inline; filename=logo.png')
+                res.setHeader('content-name','image/jpg')
+                res.setHeader('last','sita')
+                res.sendFile(path.join(contentstack.config.get('path').theme,'public','images','logo-company.png'))
+                /*res.set({
                     'content-disposition': 'inline; filename=owl%20pic.jpg',
                     'name':'testing',
                     'last':'ravan',
                     'content-name':'image/gif'
                 })
-               res.send("Hello User Demo")
+               res.send("Hello User Demo")*/
 
             } catch(err) {
                 next(err);
